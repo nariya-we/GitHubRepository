b678fc184f521a287ee47a6d2f06e3a36a8ad642
nariya-we <andreeva.ng@dvfu.ru>
2025-10-15 21:27:01 +1000

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const username = 'Badand'
+const App = () => {
+    return(
+        <div>
+            <h1>What</h1>
+            <p> Hi, {username}! </p>
+        </div>
+    )
+}
+
+export default App
